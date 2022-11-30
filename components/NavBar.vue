9306e06ae5ea7ae0cc1849b7ae2a94639bd93221
Hassan Aguezoum <haguezoum@gmail.com>
2022-11-30 09:11:52 +0000

    nav bar

---
+++ components/NavBar.vue
@@ -0,0 +1,26 @@
+<template>
+    <nav class="w-full p-4 px-20 header-nav">
+        <div class="container flex justify-between items-center">
+            <div class="block text-3xl ">
+                LeeVnt
+            </div>
+            <div class="link">
+                <ul class="sm:hidden lg:flex justify-between items-center" >
+                    <router-link to="/med" class="p-2 m-2 ">
+                        LeeVnt
+                    </router-link>
+                    <li class="p-2 m-2 ">About</li>
+                    <li class="p-2 m-2 ">Contact</li>
+                </ul>
+            </div>
+            <div class="actionButton">
+                <button>Click here !</button>
+            </div>
+        </div>
+    </nav>
+</template>
+<script>
+</script>
+<style lang="css" scoped>
+
+</style>
