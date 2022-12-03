c14d3a55b89489c80b8aa5f79138e62a2d3c7722
Hassan Aguezoum <haguezoum@gmail.com>
2022-12-03 17:07:53 +0000

    rename Nuxt build directory

---
+++ nuxtDir/components/index.js
@@ -0,0 +1,35 @@
+export { default as DropDownMunu } from '../../components/DropDownMunu.vue'
+export { default as Header } from '../../components/Header.vue'
+export { default as Hero } from '../../components/Hero.vue'
+export { default as NavBar } from '../../components/NavBar.vue'
+
+// nuxt/nuxt.js#8607
+function wrapFunctional(options) {
+  if (!options || !options.functional) {
+    return options
+  }
+
+  const propKeys = Array.isArray(options.props) ? options.props : Object.keys(options.props || {})
+
+  return {
+    render(h) {
+      const attrs = {}
+      const props = {}
+
+      for (const key in this.$attrs) {
+        if (propKeys.includes(key)) {
+          props[key] = this.$attrs[key]
+        } else {
+          attrs[key] = this.$attrs[key]
+        }
+      }
+
+      return h(options, {
+        on: this.$listeners,
+        attrs,
+        props,
+        scopedSlots: this.$scopedSlots,
+      }, this.$slots.default)
+    }
+  }
+}
