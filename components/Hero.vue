d1d0a2c4717cc357f4e75b404978925ec58da758
Hassan Aguezoum <haguezoum@gmail.com>
2022-12-05 16:22:40 +0000

    add layer bg

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,8 +1,10 @@
 <template>
-    <div>
-        <span class="border-4 border-red-600">
-            Hello Woeld
-        </span>
+    <div class="hero w-full h-1/2 bg-cover flex justify-center items-center bg-fixed bg-gradient-to-b from-gray-900 purpel- to-gray-700 " >
+            <div class="content w-full h-full container mx-auto">
+                <h1 class="title">LeeVnT</h1>
+                <p class="description text-white">GeeksBlaBla is a community initiative, to discuss, highlight and share the latest IT topics in Moroccan Darija.</p>
+            </div>
+            <!-- <svg class="absolute inset-x-0 -bottom-1 text-white" viewBox="0 0 1160 163"><path fill="currentColor" d="M-164 13L-104 39.7C-44 66 76 120 196 141C316 162 436 152 556 119.7C676 88 796 34 916 13C1036 -8 1156 2 1216 7.7L1276 13V162.5H1216C1156 162.5 1036 162.5 916 162.5C796 162.5 676 162.5 556 162.5C436 162.5 316 162.5 196 162.5C76 162.5 -44 162.5 -104 162.5H-164V13Z"></path></svg> -->
     </div>
 </template>
 <script>
@@ -13,27 +15,16 @@ export default{
         }
     },
     computed:{
-        grabPaths: function()
-        {
-            return (document.querySelectorAll('#leevntLoading path'));
-        }
+    
     },
     methods: {
-        stroks()
-        {
-            const paths = this.grabPaths();
-            paths.forEach(element=>{
-                let style = element.style;
-                let delay = Math.floor(Math.random()*3);
-                style.strokeDasharray=`${element.getTotalLength()}`
-                style.strokeDashoffset=`${element.getTotalLength()}`
-                style.animation=`SVG-anim 8s ease forwards ${delay}s`;
-            });
-            //return paths;
-        }
+
     },
     created() {
-        //console.log(this.grabPaths());
     },
 }
 </script>
+
+<style lang="css" scoped>
+   
+</style>
--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,27 +1,22 @@
 <template>
-    <div class="w-full p-2 px-4 flex justify-between items-center text-white bg-gray-900 relative">
-        <div class="rightNav flex justify-center items-center p-5">
-            <span class="title text-2xl font-semibold text-gray-200">
-                LeeEvent
-            </span>
-        </div>
-        <div class="leftNav">
-            <Transition name="fade">
-                <div class="routelinks absolute w-full left-0 top-20 bg-gray-900 text-gray-200 transition duration-700 ease-in-out" v-show="openMenu" >
-                        <ul class="links flex justify-center flex-col px-8 py-10 pt-5" >
-                            <TransitionGroup  name="extraMenu" >
+    <div class="w-full px-5 text-white bg-gray-900 relative">
+        <div class="container mx-auto flex justify-between items-center">
+            <div class="rightNav flex justify-center items-center p-5">
+                <span class="title text-2xl font-semibold text-gray-200">
+                    LeeVenT
+                </span>
+            </div>
+            <div class="leftNav w-auto lg:w-full">
+                <Transition name="mainMenu">
+                    <div :class="{'hidden':!openMenu, 'fixed':openMenu}"  class="routelinks w-full left-0 top-16 bg-gray-900 text-gray-200 transition duration-700 ease-in-out">
+                        <div class="lg:hidden">
+                            <TransitionGroup  name="extraMenu" class="links flex justify-center flex-col px-8 py-10 pt-5" tag="ul">
                                 <li v-for="(link, index) in links" :key="index" class="px-2 py-3 bg-inherit hover:bg-gradient-to-r from-gray-700 to-gray-900 transition duration-700 ease-in-out rounded-lg " v-show="openMenu">
                                     {{link}}
                                 </li> 
-                                
                             </TransitionGroup>
-                            <button role="li" class="rounded-lg border-2 px-5 py-2 border-gray-200 bg-inherit flex items-center" @click="(open = !open)" >
-                                    <span> More</span>
-                                    <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': open, 'rotate-0': !open}" class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                            </button>
-                        </ul>
-                    <!-- <TransitionGroup v-show="open"  name="extraMenu"  class="absolute right-0 w-full md:max-w-screen-sm md:w-screen mt-2 origin-top-right" > -->
-                        <div class="extraMenu hidden px-2 pt-2 pb-4 bg-white rounded-md shadow-lg dark-mode:bg-gray-700">
+                        </div>                            
+                        <div class="extraMenu hidden px-2 pt-2 pb-4 rounded-md shadow-lg border bg-inherit">
                             <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
                                 <a class=" flex row items-start rounded-lg bg-transparent p-2 dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">
                                 <div class="bg-teal-500 text-white rounded-lg p-3">
@@ -33,7 +28,7 @@
                                 </div>
                                 </a>
 
-                                <a class="flex flex row items-start rounded-lg bg-transparent p-2 dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">
+                                <a class="flex row items-start rounded-lg bg-transparent p-2 dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">
                                 <div class="bg-teal-500 text-white rounded-lg p-3">
                                     <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="md:h-6 md:w-6 h-4 w-4"><path d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"></path></svg>
                                 </div>
@@ -54,24 +49,34 @@
                                 </a>
                             </div>
                         </div>
-                    <!-- </TransitionGroup> -->
+                    </div>
+                </Transition>
+                <!-- larger screen -->
+                <Transition name="navlinks">
+                    <div class="routelinks hidden lg:block w-full left-0 top-0 bg-gray-900 text-gray-200 transition duration-700 ease-in-out"  >
+                        <TransitionGroup  name="extraMenu" class="links flex justify-center items-center flex-row p-1 gap-x-6" tag="ul">
+                            <li v-for="(link, index) in links" :key="index" class="px-6 flex items-center justify-start gap-x-2 py-3 bg-inherit hover:bg-gradient-to-b from-gray-700 to-gray-900 transition duration-700 ease-in-out rounded-lg">
+                                <span v-html="link.icone"></span>
+                                <span>{{link.key}}</span>
+                            </li> 
+                            
+                        </TransitionGroup>
+                    </div>
+                </Transition>
+                <div class="toggelButton lg:hidden top-1/2 transition ">
+                    <span class="open focus:rotate-90 transition-all duration-700 text-white right-0">
+                        <!-- open -->
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" @click=toggel() >
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
+                        </svg>
+                        <!-- close -->
+                        <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6" @click=toggel() v-if="openMenu">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg> -->
+                    </span>
                 </div>
-            </Transition>
-            <div class="toggelButton">
-                <span class="open focus:rotate-90 transition-all duration-700 text-white" @click="(openMenu = !openMenu)" v-show="!openMenu">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25" />
-                    </svg>
-                </span>
-                <span class="close focus:-rotate-90 transition-all duration-700 text-white" @click="(openMenu = !openMenu)" v-show="openMenu">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </span>
-
             </div>
         </div>
-
     </div>
 </template>
 
@@ -79,9 +84,21 @@
 export default{
     data(){
         return{
-                links:["About","LeeVenT","Contact"],
+                links:[
+                    {key:"Home", icone:`<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" /></svg>`},
+                    {key:"Blog", icone:``},
+                    {key:"News", icone:``},
+                    {key:"Blog", icone:``},
+
+                    // "Blog","News", "Podcast", "About"
+                ],
                 openMenu:false,
                 open:false
+        }
+    },
+    methods:{
+        toggel(){
+            this.openMenu = !this.openMenu;
         }
     }
 }
@@ -89,7 +106,7 @@ export default{
 <style lang="css" scoped>
 .extraMenu-enter-active,
 .extraMenu-leave-active {
-  transition: all 1s ease;
+  transition: all 1s ease-in-out;
 }
 .extraMenu-enter-from,
 .extraMenu-leave-to {
@@ -97,12 +114,9 @@ export default{
   transform: translateX(30px);
 }
 /****************** */
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity 0.5s ease-in-out;
-}
 
-.fade-enter-from,
-.fade-leave-to {
-  opacity: 0;
+.fade-inter,
+.fade-leave-active {
+    top:120%;
+    transition: all 1s ease-in-out;
 }
