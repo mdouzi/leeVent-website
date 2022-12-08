ddf581e3016636822126de344c45b06b3a134921
Hassan Aguezoum <haguezoum@gmail.com>
2022-12-08 15:51:50 +0000

    add comonents overimage and blogprototype

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,14 @@
 <template>
    <div class="bg-gradient-to-b from-gray-900 purpel- to-gray-700 w-full h-screen">
       <Hero/>
-      <segments/>
+      <Segments/>
+      <OverImage/>
+      <BlogProto/>
    </div>
 </template>
 
 <script>
-import Segments from '~/components/Segments.vue'
 export default {
-  components: { Segments },
   name: 'IndexPage'
 }
 </script>
