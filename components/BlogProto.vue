c15045090a0ada5bef54f8af099ab845656dbf3d
Hassan Aguezoum <haguezoum@gmail.com>
2022-12-09 09:06:47 +0000

    add blog prototype comoponent

---
+++ components/BlogProto.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="w-full">
+        <div class="container flex flex-col justify-between">
+            <div class="title w-full p-4 text-3xl text-center ">
+                Blog
+            </div>
+            <div class="flex justify-between items-center">
+                <div class="media">
+                    <img src="https://images.unsplash.com/photo-1542435503-956c469947f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2874&q=80" alt="">
+                </div>
+                <div class="description"></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
