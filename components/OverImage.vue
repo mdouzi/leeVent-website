859f81f8857af35e1045326432d19ef9f765306f
Hassan Aguezoum <haguezoum@gmail.com>
2022-12-09 09:07:26 +0000

    add overimage component

---
+++ components/OverImage.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="w-full bg-cover bg-center bg-origin-border h-96" style="background-image: url('https://64.media.tumblr.com/tumblr_m6lpjxTh1E1r4mh0bo1_500.gifv')">
+
+    </div>
+</template>
+
+<script>
+export default {
+   data(){
+    return{
+
+    }
+   }
+}
+</script>
+
+<style lang="css" scoped>
+/* // .main{
+//     background-image: url('https://64.media.tumblr.com/cdf2262e4751d1dc481ff4045095862c/tumblr_n7jttrwZ6d1qza1qzo1_r2_500.gifv');
+// } */
+</style>
